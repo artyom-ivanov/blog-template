831e544cd76dce5a808c8b46d212e504c1792c90
Artyom Ivanov <artem@anmedio.ru>
2021-09-09 14:53:49 +0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Gatsby Blog",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
